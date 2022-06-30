3e4697d8759f598abd46d6a32f4a238d56ef33c7
Zdzislaw Pietrewicz <zdzislaw.pietrewicz@gmail.com>
2022-06-30 13:37:26 +0200

    restructiring the class data after analyse

--- data/Account.java
+++ data/Account.java
@@ -2,40 +2,8 @@ import java.math.BigInteger;
 
 public class Account {
     private int accountIdentifier;
-    private int pin;
-    private long creditCardNumber;
+    private CreditCard creditCard;
 
 
-    public Account(int accountIdentifier, int pin, long creditCardNumber) {
-        this.accountIdentifier = accountIdentifier;
-        this.pin = pin;
-        this.creditCardNumber = creditCardNumber;
-    }
 
-    Account(){
-    }
-
-    public int getAccountIdentifier() {
-        return accountIdentifier;
-    }
-
-    public void setAccountIdentifier(int accountIdentifier) {
-        this.accountIdentifier = accountIdentifier;
-    }
-
-    public int getPin() {
-        return pin;
-    }
-
-    public void setPin(int pin) {
-        this.pin = pin;
-    }
-
-    public long getCreditCardNumber() {
-        return creditCardNumber;
-    }
-
-    public void setCreditCardNumber(long creditCardNumber) {
-        this.creditCardNumber = creditCardNumber;
-    }
 }
--- data/Customer.java
+++ data/Customer.java
@@ -1,2 +1,25 @@
 public class Customer {
+    private Account account;
+    private Address address;
+
+    public Customer(Account account, Address address) {
+        this.account = account;
+        this.address = address;
+    }
+
+    public Account getAccount() {
+        return account;
+    }
+
+    public void setAccount(Account account) {
+        this.account = account;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
 }
